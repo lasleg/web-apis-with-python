063018e14ac27643d33d2e9c936bc829ae56f62b
lasleg <lasleg@yandex.ru>
2022-11-12 18:38:24 +1000

    completed app

--- bin/filters.py
+++ bin/filters.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter
+from ast import literal_eval
 import io
 
 
@@ -11,5 +12,11 @@ def apply_filter(file: object, filter: str) -> object:
     4. Convert the PIL Image object to file object
     5. Return the file object
     """
+    image = Image.open(file)
+    image = image.filter(eval(f'ImageFilter.{filter.upper()}'))
 
-    pass
+    file = io.BytesIO()
+    image.save(file, 'JPEG')
+    file.seek(0)
+
+    return file
